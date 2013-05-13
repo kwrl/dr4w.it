2dc96c5aabd705374008c8719d2a2ea95aa837a3
Joachricar <joachimcarlsen@gmail.com>
2013-05-13 22:36:30 +0200

    Added tool menu. canvas is 100%

--- drawit/drawit.js
+++ drawit/drawit.js
@@ -1,21 +1,27 @@
 /*
-Required fields and functions of a tool
 
-class Tool {
-    String uname; // unique name
-    String displayName;
 
-    onmousemove(event);
-    onmouseout(event);
-    onmouseup(event);
-    onmousedown(event);
+Tool template
 
-    // Draw data from its own tool
-    // element = data received from a websocket
-    drawElement(element);
-}
+
+var ToolName = function() {}
+
+ToolName.prototype = {
+
+    uname : "ToolName",
+    displayName : "Tool Name",
+    icon: "IconURL",
+
+    onmousedown : function(e) {},
+    onmouseup : function(e) {},
+    drawElement : function(element) { },
+    onmousemove : function(e) {},
+    onmouseout : function(e) {},
+    getSettingsMenu : function() {}
+
+};
+
 */
-
 function Drawit() {
     this.tools = new Array();
     this.drawQueue = new Array();
@@ -34,11 +40,19 @@ Drawit.prototype.addTool = function (tool) {
     this.tools[tool.displayName] = tool;
 
     // Add new button to the "toolbar"
-    var newbtn = $('<input type="button" value="' + tool.displayName + '">');
+    var newbtn = $('<input type="button" title="' + tool.displayName + '">');
     $(newbtn).click(function() {
         // This name must be the same as in the "tools"-map
         drawit.setTool(tool.displayName);
-    });
+
+        $(".tool_button").each(function() {
+            $(this).css('border', 'none');
+        });
+
+        $(this).css('border', 'solid 1px gray');
+    }).addClass('tool_button')
+    .css('background-image', "url(" + tool.icon + ")");
+
     $('#tools').append(newbtn);
 }
 
@@ -59,20 +73,15 @@ SquareTool.prototype = {
     uname       : "SquareTool",
     displayName : "Square Tool",
     fill        : false,
+    icon        : "images/icons/square.png",
 
     onmousedown: function(e) {
         var point = getMousePosition(e);
         this.mousedownx = point.x;
         this.mousedowny = point.y;
     },
-
     onmouseup: function(e) {
         var point = getMousePosition(e);
-
-        var ctx = document.getElementById('canvas').getContext('2d');
-        
-
-
         if(this.fill) {
             ctx.fillRect(
                 this.mousedownx, 
@@ -90,38 +99,33 @@ SquareTool.prototype = {
             );
         }
     },
-
     onmousemove: function(e) {
         // do nothing :S
         // Kanskje update og vise hvordan det vil se ut?
-        // Hard t
     },
-
     onmouseout: function(e) {
         // Should return drawing-data as json.
     },
-
     drawElement: function(element) {
 
     },
-
     getSettingsMenu: function() {
-        console.log(this.menudiv);
-        if(this.menudiv == undefined) {
-            this.menudiv = document.createElement("div");
-            var label = jQuery("<label>").text("Fill");
+        var menudiv = document.createElement("div");
+        var label = jQuery("<label>").text("Fill");
 
-            // Because when we enter a function, "this" is the function itself...
-            var self = this;
-
-            var input = $('<input />', { type: 'checkbox', value: 'Fill' })
-            .change(function() {
+        // Because when we enter a function, "this" is the function itself...
+        var self = this;
+        var input = $('<input />', 
+        { 
+            type: 'checkbox', 
+            checked: self.fill,
+            change: function() {
                 self.fill = !self.fill;
-            }).appendTo(label);
-            $(label).appendTo(this.menudiv);
-        }
+            }
+        }).appendTo(label);
+        $(label).appendTo(menudiv);
 
-        return this.menudiv;
+        return menudiv;
     }
 };
 
@@ -129,94 +133,148 @@ SquareTool.prototype = {
 //---------------------------------------------------------
 //---------------------------------------------------------
 // Circle-tool
-function CircleTool() {
-    this.uname = "CircleTool";
-    this.fill = false;
-    this.displayName = "Circle Tool";
-}
+var CircleTool = function() {}
+CircleTool.prototype = {
+    uname : "CircleTool",
+    fill    : false,
+    fillbg  : false,
+    displayName : "Circle Tool",
+    icon        : "images/icons/circle.png",
 
-CircleTool.prototype.onmousedown = function(e) {
-    var point = getMousePosition(e);
-    this.mousedownx = point.x;
-    this.mousedowny = point.y;
-}
+    onmousedown : function(e) {
+        var point = getMousePosition(e);
+        this.mousedownx = point.x;
+        this.mousedowny = point.y;
+    },
+    onmouseup : function(e) {
+        var point = getMousePosition(e);
+        // Giddsje endre på det andre =\
+        
+        var x = this.mousedownx;
+        var y = this.mousedowny;
+        var w = point.x - x;
+        var h = point.y - y;
 
-CircleTool.prototype.onmouseup = function(e) {
-    var ctx = document.getElementById('canvas').getContext('2d');
-    
-    var point = getMousePosition(e);
-    // Giddsje endre på det andre =\
-    
-    var x = this.mousedownx;
-    var y = this.mousedowny;
-    var w = point.x - x;
-    var h = point.y - y;
+        var kappa = .5522848,
+            ox = (w / 2) * kappa, // control point offset horizontal
+            oy = (h / 2) * kappa, // control point offset vertical
+            xe = x + w,           // x-end
+            ye = y + h,           // y-end
+            xm = x + w / 2,       // x-middle
+            ym = y + h / 2;       // y-middle
 
-    var kappa = .5522848,
-        ox = (w / 2) * kappa, // control point offset horizontal
-        oy = (h / 2) * kappa, // control point offset vertical
-        xe = x + w,           // x-end
-        ye = y + h,           // y-end
-        xm = x + w / 2,       // x-middle
-        ym = y + h / 2;       // y-middle
+        ctx.beginPath();
+        ctx.moveTo(x, ym);
+        ctx.bezierCurveTo(x, ym - oy, xm - ox, y, xm, y);
+        ctx.bezierCurveTo(xm + ox, y, xe, ym - oy, xe, ym);
+        ctx.bezierCurveTo(xe, ym + oy, xm + ox, ye, xm, ye);
+        ctx.bezierCurveTo(xm - ox, ye, x, ym + oy, x, ym);
+        ctx.closePath();
+        ctx.stroke();
 
-    ctx.beginPath();
-    ctx.moveTo(x, ym);
-    ctx.bezierCurveTo(x, ym - oy, xm - ox, y, xm, y);
-    ctx.bezierCurveTo(xm + ox, y, xe, ym - oy, xe, ym);
-    ctx.bezierCurveTo(xe, ym + oy, xm + ox, ye, xm, ye);
-    ctx.bezierCurveTo(xm - ox, ye, x, ym + oy, x, ym);
-    ctx.closePath();
-    ctx.stroke();
-
-    if(this.fill) {
-        ctx.fill();
-    }
-}
-
-CircleTool.prototype.drawElement = function(element) {
-    // element = JSON-object
-    // Execute drawing as 
-}
-
-CircleTool.prototype.onmousemove = function(e) {
-    // do nothing :S
-}
-
-CircleTool.prototype.onmouseout = function(e) {
-    // nutn elns.
-    // Should return drawing-data as json.
-}
-
-CircleTool.prototype.getSettingsMenu = function() {
-    if(this.menudiv == undefined) {
-        this.menudiv = document.createElement("div");
-        var label = jQuery("<label>").text("Fill");
+        if(this.fill) {
+            if(this.fillbg) {
+                setFillColor(backgroundColor);
+            }
+            else {
+                setFillColor(foregroundColor);
+            }
+            ctx.fill();
+        }
+    },
+    drawElement : function(element) {
+        // element = JSON-object
+        // Execute drawing as 
+    },
+    onmousemove : function(e) {
+        // do nothing :S
+    },
+    onmouseout : function(e) {
+        // nutn elns.
+        // Should return drawing-data as json.
+    },
+    getSettingsMenu : function() {
+        var menudiv = document.createElement("div");
+        var lblFillShape        = jQuery("<label>").text("Fill");
+        var lblFillBackground   = jQuery("<label>").text("Fill bkg");
 
         // Because when we enter a function, "this" is the function itself...
         var self = this;
 
-        var input = $('<input />', { type: 'checkbox', value: 'Fill' })
-        .change(function() {
-            self.fill = !self.fill;
-        }).appendTo(label);
-        $(label).appendTo(this.menudiv);
-    }
+        var checkboxFillShape = $('<input />', 
+        { 
+            type: 'checkbox',
+            checked: this.fill,
+            change: function() {
+                console.log("change fill");
+                self.fill = !self.fill;
+                if(self.fill) {
+                    $(lblFillBackground).show();
+                }
+                else {
+                    $(lblFillBackground).hide();
+                }
+            }
+        }).appendTo(lblFillShape);
+        
+        var cbFillBackground = $('<input />', {
+            type: 'checkbox',
+            checked: this.fillbg,
+            change: function() {
+                console.log("change fillbg");
+                self.fillbg = !self.fillbg;
+            }
+        }).appendTo(lblFillBackground);
 
-    return this.menudiv;
+        $(lblFillShape).appendTo(menudiv);
+        $(lblFillBackground).appendTo(menudiv);
+        
+        if(!this.fill) {
+            $(lblFillBackground).hide();
+        }
+        return menudiv;
+    }
+};
+
+function setFillColor(color) {
+    ctx.fillStyle = "#" + color;
+}
+
+function setStrokeColor(color) {
+    ctx.strokeStyle = "#" + color;
 }
 
 //---------------------------------------------------------
 //---------------------------------------------------------
 //---------------------------------------------------------
+//Pencil
+var PencilTool = function() {}
 
+PencilTool.prototype = {
+
+    uname : "PencilTool",
+    displayName : "Pencil Tool",
+    icon: "images/icons/pencil.png",
+
+    onmousedown : function(e) {},
+    onmouseup : function(e) {},
+    drawElement : function(element) { },
+    onmousemove : function(e) {},
+    onmouseout : function(e) {},
+    getSettingsMenu : function() {}
+    
+};
+
+//---------------------------------------------------------
+//---------------------------------------------------------
+//---------------------------------------------------------
 // Selve drawit-objektet.
 // Inneholder liste over tools og diverse annet kult
 var drawit = new Drawit();
 
 var squareTool = new SquareTool();
 var circleTool = new CircleTool();
-
+var pencilTool = new PencilTool();
 // Noen placeholders.
 // Disse skal overrides av toolsene
 // Blir kalt fra canvas-eventsene
@@ -225,12 +283,18 @@ var canvasmousemove = function(e) {},
     canvasmouseup = function(e){},
     canvasmousedown = function(e){};
 
+var canvas;
+var ctx;
+
+var foregroundColor;
+var backgroundColor;
+
 $(function() {
-    var canvas = document.getElementById('canvas'),
-            context = canvas.getContext('2d');
+    canvas = document.getElementById('canvas'),
+            ctx = canvas.getContext('2d');
 
     // resize the canvas to fill browser window dynamically
-    window.addEventListener('resize', resizeCanvas, false);
+    // window.addEventListener('resize', resizeCanvas, false);
 
     function resizeCanvas() {
             canvas.width = window.innerWidth;
@@ -254,9 +318,53 @@ $(function() {
         canvasmousedown(e);
     }
 
-
     drawit.addTool(squareTool);
     drawit.addTool(circleTool);
+    drawit.addTool(pencilTool);
+    $("#colorSwapper").click(function() {
+        var temp = foregroundColor;
+        foregroundColor = backgroundColor;
+        backgroundColor = temp;
+
+        $("#foregroundColor").val(foregroundColor);
+        $("#foregroundColor").css('background-color', foregroundColor);
+        $("#backgroundColor").val(backgroundColor);
+        $("#backgroundColor").css('background-color', backgroundColor);
+
+        setFillColor(backgroundColor);
+        setStrokeColor(foregroundColor);
+    });
+
+    $("#foregroundColor").change(function() {
+        foregroundColor = this.value;
+        setStrokeColor(foregroundColor);
+    });
+
+    $("#backgroundColor").change(function() {
+        backgroundColor = this.value;
+        setFillColor(backgroundColor);
+    });
+
+    backgroundColor = $("#backgroundColor").val();
+    foregroundColor = $("#foregroundColor").val();
+
+    $( "#draggable" ).draggable({ handle: "p" });
+
+    $("#toolMenuWrapperMinimizeButton").click(function() {
+        $("#toolMenuWrapper").toggle('fast');
+    });
+
+    $("#toolMenuHeader").mousedown(function(e) {
+        this.point = getMousePosition(e);
+    });
+
+    $("#toolMenuHeader").mouseup(function(e) {
+        var p = getMousePosition(e);
+
+        if(this.point.x == p.x && this.point.y == p.y) {
+            $("#toolMenuWrapper").toggle('fast');
+        }
+    });
 })();
 
 function Point(x, y) {
@@ -265,7 +373,7 @@ function Point(x, y) {
 }
 
 function getMousePosition(e) {
-    var x = e.pageX  - canvas.offsetLeft;
+    var x = e.pageX - canvas.offsetLeft;
     var y = e.pageY - canvas.offsetTop;
     
     return new Point(x, y);
